d2832a1c70bd83d83fe104c66686e77b62878122
root <1443548569@qq.com>
2023-04-23 22:04:10 +0800

    Initial commit

---
+++ uaa-provider/src/main/java/com/czy/uaa/UaaApplication.java
@@ -0,0 +1,13 @@
+package com.czy.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class UaaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UaaApplication.class, args);
+    }
+}
